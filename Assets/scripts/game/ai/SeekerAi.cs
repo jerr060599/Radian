97b1c86015756fd7ea2610ca629f0957441b26b0
Jimmy Shi <Jimmyshi360@gmail.com>
2016-07-23 18:15:10 -0400

    AddedFlippingAnimations

--- Assets/scripts/game/ai/SeekerAi.cs
+++ Assets/scripts/game/ai/SeekerAi.cs
@@ -4,17 +4,30 @@ using System.Collections;
 public class SeekerAi : BasicEnemy
 {
     public float agroRadius = 2f, parkRadius = 1.1f, maxImpulse = 1f;
+	bool agroStay=false;
     // Update is called once per frame
     protected void Update()
     {
         agro = (CharCtrl.script.pysc.position - pysc.position).sqrMagnitude <= agroRadius * agroRadius;
-        if (agro)
+		if (agro || agroStay)
         {
+			agroStay = true;
             Vector2 dPos = CharCtrl.script.pysc.position - pysc.position;
-            if (dPos.sqrMagnitude > parkRadius * parkRadius)
-                pysc.AddForce(Vector2.ClampMagnitude(((CharCtrl.script.pysc.position - pysc.position).normalized * walkSpeed - pysc.velocity) * pysc.mass, maxImpulse), ForceMode2D.Impulse);
-            else
-                pysc.AddForce(Vector2.ClampMagnitude(-pysc.velocity * pysc.mass, maxImpulse), ForceMode2D.Impulse);
+			if (dPos.sqrMagnitude > parkRadius * parkRadius) {
+				pysc.AddForce (Vector2.ClampMagnitude (((CharCtrl.script.pysc.position - pysc.position).normalized * walkSpeed - pysc.velocity) * pysc.mass, maxImpulse), ForceMode2D.Impulse);
+				if (dPos.x <= 0)
+					GetComponent<Animator> ().Play ("EnemyWalk");
+				else
+					GetComponent<Animator> ().Play ("EnemyWalkFlipped");
+				
+			} else {
+				pysc.AddForce (Vector2.ClampMagnitude (-pysc.velocity * pysc.mass, maxImpulse), ForceMode2D.Impulse);
+				if (dPos.x <=0 )
+				GetComponent<Animator> ().Play ("EnemyMelee");
+				else
+					GetComponent<Animator> ().Play ("EnemyMeleeFlipped");
+			}
+	
         }
         else
             pysc.AddForce(Vector2.ClampMagnitude(-pysc.velocity * pysc.mass, maxImpulse), ForceMode2D.Impulse);
