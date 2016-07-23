cc28994727ae7458527952a5429f1ed8a055dead
jerry060599 <jerry060599@gmail.com>
2016-07-23 10:54:06 -0400

    Upgraded autosnap script + new blank tile

--- Assets/scripts/game/AutoSnap.cs
+++ Assets/scripts/game/AutoSnap.cs
@@ -4,7 +4,7 @@ using System.Collections;
 [ExecuteInEditMode]
 public class AutoSnap : MonoBehaviour
 {
-    public int xOffset = 0, yOffset = 0;
+    public int xOffset = 0, yOffset = 0, dpu = 40;
     // Update is called once per frame
     void Update()
     {
@@ -13,8 +13,8 @@ public class AutoSnap : MonoBehaviour
             float a = transform.localRotation.eulerAngles.z / 180f * Mathf.PI;
             float ux = Mathf.Cos(a);
             float uy = Mathf.Sin(a);
-            float xp = Mathf.Floor((ux * transform.localPosition.x + uy * transform.localPosition.y + 1f / 80f) * 40f) / 40 + xOffset;
-            float yp = Mathf.Floor((-uy * transform.localPosition.x + ux * transform.localPosition.y + 1f / 80f) * 40f) / 40 + yOffset;
+            float xp = Mathf.Floor((ux * transform.localPosition.x + uy * transform.localPosition.y + 0.5f / dpu) * dpu) / dpu + xOffset;
+            float yp = Mathf.Floor((-uy * transform.localPosition.x + ux * transform.localPosition.y + 0.5f / dpu) * dpu) / dpu + yOffset;
             transform.localPosition = new Vector3(ux * xp - uy * yp, uy * xp + ux * yp, transform.localPosition.z);
         }
     }
