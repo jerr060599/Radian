49fa87a45f3c9d2d295b9eb48cd5597ec0b60ec4
jerry060599 <jerry060599@gmail.com>
2016-07-24 21:20:35 -0400

    Tried out sublime and changed... something

--- Assets/scripts/game/Activatable.cs
+++ Assets/scripts/game/Activatable.cs
@@ -1,57 +1,60 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public abstract class Activatable : MonoBehaviour
 {
-	public bool activated = false, playerActivatable = true;
-	public GameObject chainedActivatable = null, radioGroup = null;
-	public Sprite activatedTex, deactivatedTex;
-	protected RadioGroup rg = null;
-	protected Activatable nextActivatable = null;
-	protected SpriteRenderer sr;
+    public bool activated = false, playerActivatable = true;
+    public GameObject chainedActivatable = null, radioGroup = null;
+    public Sprite activatedTex, deactivatedTex;
+    protected RadioGroup rg = null;
+    protected Activatable nextActivatable = null;
+    protected SpriteRenderer sr;
 
-	void Start ()
-	{
-		sr = GetComponent<SpriteRenderer> ();
-		if (chainedActivatable != null)
-			nextActivatable = chainedActivatable.GetComponent<Activatable> ();
-		if (radioGroup != null)
-			rg = radioGroup.GetComponent<RadioGroup> ();
-		if (rg != null)
-			rg.register (this);
-		init ();
-	}
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        if (chainedActivatable != null)
+            nextActivatable = chainedActivatable.GetComponent<Activatable>();
+        if (radioGroup != null)
+            rg = radioGroup.GetComponent<RadioGroup>();
+        if (rg != null)
+            rg.register(this);
+        init();
+    }
 
-	public virtual void init ()
-	{
-		if (activated)
-			sr.sprite = activatedTex;
-		else
-			sr.sprite = deactivatedTex;
-	}
+    public virtual void init()
+    {
+        if (activated)
+            sr.sprite = activatedTex;
+        else
+            sr.sprite = deactivatedTex;
+    }
 
-	public virtual void activate (CharCtrl player)
-	{
-		if (activated) {
-			sr.sprite = deactivatedTex;
-			activated = false;
-			onDeactivation (player);
-		} else {
-			sr.sprite = activatedTex;
-			activated = true;
-			onActivation (player);
-			if (rg != null)
-				rg.checkout (this, player);
-		}
-		if (nextActivatable != null)
-			nextActivatable.activate (player);
-	}
+    public virtual void activate(CharCtrl player)
+    {
+        if (activated)
+        {
+            sr.sprite = deactivatedTex;
+            activated = false;
+            onDeactivation(player);
+        }
+        else
+        {
+            sr.sprite = activatedTex;
+            activated = true;
+            onActivation(player);
+            if (rg != null)
+                rg.checkout(this, player);
+        }
+        if (nextActivatable != null)
+            nextActivatable.activate(player);
+    }
 
-	public virtual void onActivation (CharCtrl player)
-	{
-	}
+    public virtual void onActivation(CharCtrl player)
+    {
+    }
 
-	public virtual void onDeactivation (CharCtrl player)
-	{
-	}
+    public virtual void onDeactivation(CharCtrl player)
+    {
+    }
 }
