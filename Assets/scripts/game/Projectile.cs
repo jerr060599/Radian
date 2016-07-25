f3242f2f6942eb31af0f59f5111d4dac327aa66b
jerry060599 <jerry060599@gmail.com>
2016-07-25 16:05:43 -0400

    Meta changed

---
+++ Assets/scripts/game/Projectile.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour
+{
+    public int damage;
+    void OnTriggerEnter2D(Collider2D c)
+    {
+        if (c.attachedRigidbody && c.attachedRigidbody.gameObject)
+        {
+            BasicEnemy be = c.attachedRigidbody.gameObject.GetComponent<BasicEnemy>();
+            if (be)
+                be.damage(damage, BasicEnemy.RANGED_DAMAGE);
+        }
+        Destroy(gameObject);
+    }
+    public void setVelocity(Vector2 velocity)
+    {
+        GetComponent<Rigidbody2D>().velocity = velocity;
+        transform.localRotation = Quaternion.LookRotation(Vector3.forward, -velocity);
+    }
+}
