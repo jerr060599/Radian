1e5c0d892c53cf47a66726e1164f5caa24188603
Jimmy Shi <Jimmyshi360@gmail.com>
2016-07-26 22:35:25 -0400

    levels r done

---
+++ Assets/Scripts/Game/TriggerScene.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+public class TriggerScene : MonoBehaviour {
+
+	public string scene;
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if (col.gameObject.tag == "Player") {
+			SceneManager.LoadScene (scene);
+		}
+	}
+}
