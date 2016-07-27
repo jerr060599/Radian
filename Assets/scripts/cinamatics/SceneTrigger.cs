ec225f3d685df420493d9425f92a50924753f57e
jerry060599 <jerry060599@gmail.com>
2016-07-26 22:55:52 -0400

    Added scene triggers + rebalance

---
+++ Assets/scripts/cinamatics/SceneTrigger.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTrigger : MonoBehaviour {
+    public string scene;
+    void OnTriggerEnter2D(Collider2D c)
+    {
+        if (c.attachedRigidbody && c.attachedRigidbody.gameObject == CharCtrl.script.gameObject)
+            SceneManager.LoadScene(scene);
+    }
+}
