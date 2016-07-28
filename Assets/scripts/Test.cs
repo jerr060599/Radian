6139cbd70f86daf649ec016010d14cf9aab6c584
jerry060599 <jerry060599@gmail.com>
2016-07-27 21:52:05 -0400

    Added auto shadow generation under auto ordering script

---
+++ Assets/scripts/Test.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+[ExecuteInEditMode]
+public class Test : MonoBehaviour {
+    public bool activate = false;
+	// Update is called once per frame
+	void Update () {
+        if (activate)
+        {
+            Debug.Log(GetComponent<SpriteRenderer>().bounds.min);
+            activate = false;
+        }
+	}
+}
--- Assets/scripts/game/AutoOrder.cs
+++ Assets/scripts/game/AutoOrder.cs
@@ -3,12 +3,35 @@ using System.Collections;
 [ExecuteInEditMode]
 public class AutoOrder : MonoBehaviour
 {
-    public float offset = 0f;
-    public bool isStatic = false;
+    public float offset = 0f, shadowScale = 2f;
+    public bool isStatic = false, orderOnY = true, generateShadow = false;
+    public GameObject shadow = null;
+    SpriteRenderer sr = null;
     void Update()
     {
         if (isStatic && !(Application.isEditor && !Application.isPlaying))
             Destroy(this);
-        transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, (transform.localPosition.y + offset) / 100f);
+        if (!sr)
+            sr = GetComponent<SpriteRenderer>();
+        if (orderOnY)
+            transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, (transform.localPosition.y + offset) / 100f);
+        if (generateShadow && !shadow)
+        {
+            shadow = new GameObject();
+            shadow.name = "shadow";
+            shadow.transform.SetParent(gameObject.transform);
+            shadow.AddComponent<SpriteRenderer>();
+            SpriteRenderer shadowSr = shadow.GetComponent<SpriteRenderer>();
+            shadowSr.sprite = sr.sprite;
+            shadowSr.color = new Color(0f, 0f, 0f, 0.5f);
+            shadow.transform.localScale = new Vector3(1f, shadowScale, 1f);
+            float dMin = sr.bounds.extents.y - shadowSr.bounds.extents.y;
+            shadow.transform.localPosition = new Vector3(0f, -dMin, 0f);
+        }
+        if (!generateShadow && shadow)
+        {
+            DestroyImmediate(shadow);
+            shadow = null;
+        }
     }
 }
