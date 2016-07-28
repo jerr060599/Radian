6eb43f92fe6471e657f609e706323dddc3f50b16
jerry060599 <jerry060599@gmail.com>
2016-07-28 10:19:07 -0400

    Added more shadow customization

--- Assets/scripts/game/AutoOrder.cs
+++ Assets/scripts/game/AutoOrder.cs
@@ -3,14 +3,16 @@ using System.Collections;
 [ExecuteInEditMode]
 public class AutoOrder : MonoBehaviour
 {
-    public float offset = 0f, shadowScale = 2f;
-    public bool isStatic = false, orderOnY = true, generateShadow = false, updateShadowInRuntime = false;
+    public float offset = 0f, shadowScale = 1.5f, shadowOffset = 0f, shadowDarkness = 0.3f;
+    public bool isStatic = false, orderOnY = true, generateShadow = false, updateShadowInRuntime = false, refresh = false;
     public GameObject shadow = null;
     SpriteRenderer sr = null;
     void Update()
     {
         if (!sr)
             sr = GetComponent<SpriteRenderer>();
+        if (refresh)
+            genShadow();
         if (!Application.isEditor || Application.isPlaying)
         {
             if (updateShadowInRuntime && shadow)
@@ -34,6 +36,7 @@ public class AutoOrder : MonoBehaviour
     }
     void genShadow()
     {
+        refresh = false;
         if (!shadow)
         {
             shadow = new GameObject();
@@ -43,10 +46,10 @@ public class AutoOrder : MonoBehaviour
         }
         SpriteRenderer shadowSr = shadow.GetComponent<SpriteRenderer>();
         shadowSr.sprite = sr.sprite;
-        shadowSr.color = new Color(0f, 0f, 0f, 0.5f);
+        shadowSr.color = new Color(0f, 0f, 0f, shadowDarkness);
         shadow.transform.localPosition = Vector3.zero;
         shadow.transform.localScale = new Vector3(1f, shadowScale, 1f);
         float dMin = sr.bounds.extents.y - shadowSr.bounds.extents.y;
-        shadow.transform.localPosition = new Vector3(0f, -dMin, 0f);
+        shadow.transform.localPosition = new Vector3(0f, -dMin + shadowOffset, 0f);
     }
 }
