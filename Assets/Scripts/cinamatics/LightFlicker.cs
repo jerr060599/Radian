109ffd43a71e75da041672b1da558daf5bcf8fb9
Jimmy Shi <Jimmyshi360@gmail.com>
2016-07-27 21:18:21 -0400

    new tile snapping

---
+++ Assets/Scripts/cinamatics/LightFlicker.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightFlicker : MonoBehaviour {
+
+	float timeOn = 0.1f;
+	float timeOff = 0.5f;
+	private float changeTime = 0;
+
+
+	void Update() {
+		if (Time.time > changeTime) {
+			GetComponent<Light>().enabled = !GetComponent<Light>().enabled;
+			if (	GetComponent<Light>().enabled) {
+				changeTime = Time.time + timeOn;
+			} else {
+				changeTime = Time.time + timeOff;
+			}
+		}
+	}
+}
