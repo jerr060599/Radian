ad1c24097db3f38cb5adb71e447319639845b78d
jerry060599 <jerry060599@gmail.com>
2016-07-28 10:00:40 -0400

    Auto generated shadows now move with the animations + ai fixes

--- Assets/scripts/game/AutoOrder.cs
+++ Assets/scripts/game/AutoOrder.cs
@@ -4,34 +4,49 @@ using System.Collections;
 public class AutoOrder : MonoBehaviour
 {
     public float offset = 0f, shadowScale = 2f;
-    public bool isStatic = false, orderOnY = true, generateShadow = false;
+    public bool isStatic = false, orderOnY = true, generateShadow = false, updateShadowInRuntime = false;
     public GameObject shadow = null;
     SpriteRenderer sr = null;
     void Update()
     {
-        if (isStatic && !(Application.isEditor && !Application.isPlaying))
-            Destroy(this);
         if (!sr)
             sr = GetComponent<SpriteRenderer>();
+        if (!Application.isEditor || Application.isPlaying)
+        {
+            if (updateShadowInRuntime && shadow)
+            {
+                if (shadow.GetComponent<SpriteRenderer>().sprite != sr.sprite)
+                    genShadow();
+                shadow.GetComponent<SpriteRenderer>().flipX = sr.flipX;
+            }
+            if (isStatic)
+                Destroy(this);
+        }
         if (orderOnY)
             transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, (transform.localPosition.y + offset) / 100f);
         if (generateShadow && !shadow)
-        {
-            shadow = new GameObject();
-            shadow.name = "shadow";
-            shadow.transform.SetParent(gameObject.transform);
-            shadow.AddComponent<SpriteRenderer>();
-            SpriteRenderer shadowSr = shadow.GetComponent<SpriteRenderer>();
-            shadowSr.sprite = sr.sprite;
-            shadowSr.color = new Color(0f, 0f, 0f, 0.5f);
-            shadow.transform.localScale = new Vector3(1f, shadowScale, 1f);
-            float dMin = sr.bounds.extents.y - shadowSr.bounds.extents.y;
-            shadow.transform.localPosition = new Vector3(0f, -dMin, 0f);
-        }
+            genShadow();
         if (!generateShadow && shadow)
         {
             DestroyImmediate(shadow);
             shadow = null;
         }
+    }
+    void genShadow()
+    {
+        if (!shadow)
+        {
+            shadow = new GameObject();
+            shadow.name = "shadow";
+            shadow.transform.SetParent(gameObject.transform);
+            shadow.AddComponent<SpriteRenderer>();
+        }
+        SpriteRenderer shadowSr = shadow.GetComponent<SpriteRenderer>();
+        shadowSr.sprite = sr.sprite;
+        shadowSr.color = new Color(0f, 0f, 0f, 0.5f);
+        shadow.transform.localPosition = Vector3.zero;
+        shadow.transform.localScale = new Vector3(1f, shadowScale, 1f);
+        float dMin = sr.bounds.extents.y - shadowSr.bounds.extents.y;
+        shadow.transform.localPosition = new Vector3(0f, -dMin, 0f);
     }
 }
