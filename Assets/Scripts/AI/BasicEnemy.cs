4df185c92447eff6fbaa2689262fed114a518e90
jerry060599 <jerry060599@gmail.com>
2016-07-29 19:48:27 -0400

    Added knock back to enemy

--- Assets/Scripts/AI/BasicEnemy.cs
+++ Assets/Scripts/AI/BasicEnemy.cs
@@ -6,7 +6,7 @@ public class BasicEnemy : MonoBehaviour
 {
     public static readonly int DEFAULT_DAMAGE = 0, MELEE_DAMAGE = 1, RANGED_DAMAGE = 2;
     public int health = 100;
-    public float walkSpeed = 3f;
+    public float walkSpeed = 3f, damageKB = 300f;
     public bool agro = false;
     public Rigidbody2D pysc = null;
     public SpriteRenderer sr = null;
@@ -21,6 +21,8 @@ public class BasicEnemy : MonoBehaviour
     public virtual void damage(int d, int damageType = 0)
     {
         health -= d;
+        if (damageType == MELEE_DAMAGE)
+            pysc.AddForce((pysc.position - CharCtrl.script.pysc.position).normalized * damageKB);
         if (health <= 0)
             kill(damageType);
     }
