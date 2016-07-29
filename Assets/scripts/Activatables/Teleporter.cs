70c9abe538e54ddcfe8edc10005b523ae45d04df
jerry060599 <jerry060599@gmail.com>
2016-07-29 11:38:22 -0400

    Added optional animation for teleporter

--- Assets/scripts/Activatables/Teleporter.cs
+++ Assets/scripts/Activatables/Teleporter.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class Teleporter : Activatable
 {
     public GameObject destination = null;
+    public string turnOnAnimation = "", turnOffAnimation = "";
     public override void init()
     {
     }
@@ -12,5 +13,15 @@ public class Teleporter : Activatable
         if (nextActivatable != null)
             nextActivatable.activate(player);
         player.transform.position = destination.transform.position;
+    }
+    void OnTriggerEnter2D(Collider2D c)
+    {
+        if (GetComponent<Animator>() && turnOnAnimation.Length != 0)
+            GetComponent<Animator>().Play(turnOnAnimation, 0);
+    }
+    void OnTriggerExit2D(Collider2D c)
+    {
+        if (GetComponent<Animator>() && turnOffAnimation.Length != 0)
+            GetComponent<Animator>().Play(turnOffAnimation, 0);
     }
 }
