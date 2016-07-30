3222c5b827e6d6485db4642f80769d0f9aab715a
Jimmy Shi <Jimmyshi360@gmail.com>
2016-07-29 22:41:30 -0400

    added stagger in script

--- Assets/scripts/AI/RangeEnemy.cs
+++ Assets/scripts/AI/RangeEnemy.cs
@@ -6,6 +6,7 @@ public class RangeEnemy : BasicEnemy
     public float projectileAirTime = 1f, range = 10f, avoidDistance = 3f, maxImpulse = 1f, atkTime = 0.5f, seekDistance = 8f, atkAnimationLength = 1f;
     float atkTimer = float.PositiveInfinity, animationOverride = 0f;
     public GameObject projectile;
+	Vector2 dPos;
     public void fire(Transform t)
     {
         atkTimer = float.PositiveInfinity;
@@ -18,12 +19,12 @@ public class RangeEnemy : BasicEnemy
 
     void Update()
     {
-        Vector2 dPos = pysc.position - CharCtrl.script.pysc.position;
+       dPos = pysc.position - CharCtrl.script.pysc.position;
         float d = dPos.x * dPos.x + dPos.y * dPos.y;
         atkTimer -= Time.deltaTime;
         animationOverride -= Time.deltaTime;
         agro = agro ? true : d < range * range;
-        if (agro)
+		if (agro && animationOverride<0f)
         {
             if (atkTimer > atkTime)
                 if (d < avoidDistance * avoidDistance)
@@ -54,10 +55,14 @@ public class RangeEnemy : BasicEnemy
             else if (atkTimer > atkTime)
                 atkTimer = d < range * range ? atkTime : float.PositiveInfinity;
         }
+		else
+			pysc.AddForce(Vector2.ClampMagnitude(-pysc.velocity, maxImpulse) * pysc.mass, ForceMode2D.Impulse);
     }
     public override void damage(int d, int damageType = 0)
     {
         base.damage(d, damageType);
         agro = true;
+		ani.Play(dPos.x > 0f ? "stagger" : "staggerFlipped", 0);
+		animationOverride = 0.4f;
     }
 }
