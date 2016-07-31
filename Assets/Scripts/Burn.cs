5debc519ff93a21679b208ff63eeb67ac0b4bcf1
Jimmy Shi <Jimmyshi360@gmail.com>
2016-07-31 15:44:32 -0400

    awake animation

---
+++ Assets/Scripts/Burn.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Burn : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Animator> ().Play ("burning");
+	}
+
+}
