a0e3fb8c9e7531fbf3192ac03caf96989eff58b8
Jimmy Shi <Jimmyshi360@gmail.com>
2016-07-31 15:11:44 -0400

    LEVEL2 almost done

---
+++ Assets/Scripts/Activatables/AutoArrow.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoArrow : MonoBehaviour {
+	public GameObject arrowSpawn;
+	public GameObject arrow;
+	public float a,t;
+	public float velocity;
+	void Start()
+	{
+		t = 1f;
+		a = t;
+
+	}
+	void Update()
+	{
+		a -= Time.deltaTime;
+		if (a < 0.01f) {
+			GameObject arrowInstance=Instantiate (arrow, arrowSpawn.transform.position, arrowSpawn.transform.rotation) as GameObject;
+			arrowInstance.GetComponent<Projectile> ().setVelocity (Vector2.down*velocity);
+
+
+			a = t;
+		}
+		GetComponent<Animator>().Play("fire");
+	}
+
+}
