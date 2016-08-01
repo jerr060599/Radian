7d0290e35fa08c5bf82e3d6602137c0b6383ec46
jerry060599 <jerry060599@gmail.com>
2016-08-01 00:14:33 -0400

    Rebalanced blobby + bypass not working menu

--- Assets/scripts/game/EditorTools.cs
+++ Assets/scripts/game/EditorTools.cs
@@ -4,10 +4,15 @@ using System.Collections;
 public class EditorTools : MonoBehaviour
 {
     public bool resetPlayerPref = false;
+    public bool clearPrefOnStart = false;
     void Update()
     {
         if (!Application.isEditor || Application.isPlaying)
+        {
+            if (clearPrefOnStart)
+                PlayerPrefs.DeleteAll();
             Destroy(this);
+        }
         transform.position = Vector3.zero;
         if (resetPlayerPref)
             PlayerPrefs.DeleteAll();
