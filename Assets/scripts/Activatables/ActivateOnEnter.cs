083442435de67b4c559b2bf954d00295f0357410
jerry060599 <jerry060599@gmail.com>
2016-08-02 15:35:09 -0400

    Added activate on enter

---
+++ Assets/scripts/Activatables/ActivateOnEnter.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class ActivateOnEnter : Activatable
+{
+    public int validUsage = 1;
+    void OnTriggerEnter2D(Collider2D c)
+    {
+        if (c.gameObject == CharCtrl.script.gameObject)
+            activate(CharCtrl.script);
+    }
+    public override void activate(CharCtrl player)
+    {
+        validUsage--;
+        if (nextActivatable != null)
+            nextActivatable.activate(player);
+        if (validUsage <= 0)
+            Destroy(gameObject);
+    }
+}
