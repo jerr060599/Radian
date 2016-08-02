6ecde32c21d52dc4627c8f722829b6fd5f68c1ad
jerry060599 <jerry060599@gmail.com>
2016-08-02 10:08:55 -0400

    Added destroy on activate

---
+++ Assets/scripts/Activatables/DestroyOnActivate.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyOnActivate : Activatable
+{
+    public override void activate(CharCtrl player)
+    {
+        Destroy(gameObject);
+    }
+}
