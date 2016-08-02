3bc6b9b1a0e1a1f38e68816bfafc19259d1e936e
jerry060599 <jerry060599@gmail.com>
2016-08-02 11:42:13 -0400

    Added eye flash effect

--- Assets/scripts/AI/Tamahto.cs
+++ Assets/scripts/AI/Tamahto.cs
@@ -3,125 +3,110 @@ using System.Collections;
 
 public class Tamahto : BasicEnemy
 {
-    public readonly int SLAMING = 0, CRUSHING = 1, WINDING = 2, DYING = 3, SEEKING = 4, IDLE = 5;
+    public readonly int SLAMING = 0, CRUSHING = 1, WINDING = 2, DYING = 3, SEEKING = 4, IDLE = 5, STUN = 6, FADING = 7;
     public float agroRadius = 2f, parkRadius = 4f, maxImpulse = 1f, atkDistance = 1f, atkDamage = 0.2f, crushAirTime = 1f;
     public float deathTime = 2f, stunTime = 1.5f, atkWindUp = 0.5f, dashLerp = 0.1f;
-    float deathTimer = float.PositiveInfinity, stunTimer = 0f, atkTimer = 0f, airTimer = 0f;
-    int atking = -1;
+    float timer = 0f;
     int curState = 5;
-    Vector2 dPos, dashPos;
+    Vector2 dPos, tmp, lastCCPos = Vector2.zero;
     protected void Update()
     {
         uiUpdate();
+        timer -= Time.deltaTime;
+        dPos = CharCtrl.script.pysc.position - pysc.position;
         switch (curState)
         {
-            case 5:
-
+            case 6:
+                ani.Play(dPos.x < 0 ? "stun" : "stunFlipped", 0);
+                if (timer <= 0f)
+                    curState = SEEKING;
                 break;
-
-        }
-
-
-        /*
-        dPos = CharCtrl.script.pysc.position - pysc.position;
-        deathTimer -= Time.deltaTime;
-        stunTimer -= Time.deltaTime;
-        if (deathTimer <= 0f)
-            fadeAndDespawn();
-        agro = agro ? true : (CharCtrl.script.pysc.position - pysc.position).sqrMagnitude <= agroRadius * agroRadius;
-        if (agro && deathTimer > deathTime)
-        {
-            if (dPos.sqrMagnitude > parkRadius * parkRadius && atkTimer == 0f)
-            {
-                if (stunTimer <= 0f)
+            case 5:
+                pysc.AddForce(Vector2.ClampMagnitude(-pysc.velocity, maxImpulse) * pysc.mass, ForceMode2D.Impulse);
+                ani.Play("idle");
+                if (dPos.sqrMagnitude < agroRadius * agroRadius)
+                    curState = SEEKING;
+                break;
+            case 4:
+                if (dPos.sqrMagnitude > parkRadius * parkRadius)
                 {
                     pysc.AddForce(Vector2.ClampMagnitude(((CharCtrl.script.pysc.position - pysc.position).normalized * walkSpeed - pysc.velocity), maxImpulse) * pysc.mass, ForceMode2D.Impulse);
                     ani.Play(dPos.x <= 0 ? "walk" : "walkFlipped");
                 }
                 else
                 {
-                    pysc.AddForce(Vector2.ClampMagnitude(-pysc.velocity, maxImpulse) * pysc.mass, ForceMode2D.Impulse);
-                    ani.Play(dPos.x < 0f ? "idle" : "idleFlipped");
+                    lastCCPos = dPos;
+                    curState = WINDING;
+                    timer = atkWindUp;
                 }
-                atkTimer = 0f;
-            }
-            else
-            {
-                if (atking != CRUSH)
-                    pysc.AddForce(Vector2.ClampMagnitude(-pysc.velocity, maxImpulse) * pysc.mass, ForceMode2D.Impulse);
-                if (atking == -1)
+                break;
+            case 3:
+                pysc.AddForce(Vector2.ClampMagnitude(-pysc.velocity, maxImpulse) * pysc.mass, ForceMode2D.Impulse);
+                if (timer <= 0f)
                 {
-                    dashPos = dPos + CharCtrl.script.pysc.velocity * atkWindUp;
-                    atking = WINDUP;
+                    fadeAndDespawn();
+                    curState = FADING;
                 }
-                atkTimer += Time.deltaTime;
-                if (atkTimer >= atkWindUp && atking != CRUSH)
+                break;
+            case 2:
+                ani.Play(lastCCPos.x < 0f ? "atk" : "atkFlipped");
+                pysc.AddForce(Vector2.ClampMagnitude(-pysc.velocity, maxImpulse) * pysc.mass, ForceMode2D.Impulse);
+                if (timer <= 0f)
                 {
-                    atking = Random.value > 0.6f ? SLAM : CRUSH;
-                    if (atking == CRUSH)
+                    curState = Random.value > 0.6f ? SLAMING : CRUSHING;
+                    if (curState == CRUSHING)
                     {
+                        timer = crushAirTime;
                         pysc.gravityScale = 2f;
                         foreach (Collider2D c in GetComponents<Collider2D>())
                             c.enabled = false;
-                        pysc.GetComponent<Rigidbody2D>().velocity = new Vector2(dashPos.x / crushAirTime,
-                            (dashPos.y - Physics2D.gravity.y * (pysc.GetComponent<Rigidbody2D>().gravityScale) * crushAirTime * crushAirTime / 2) / crushAirTime);
-                        dashPos = Vector2.zero;
-                        airTimer = 0f;
+                        lastCCPos += CharCtrl.script.pysc.velocity * crushAirTime;
+                        pysc.GetComponent<Rigidbody2D>().velocity = new Vector2(lastCCPos.x / crushAirTime,
+                            (lastCCPos.y - Physics2D.gravity.y * (pysc.GetComponent<Rigidbody2D>().gravityScale) * crushAirTime * crushAirTime / 2) / crushAirTime);
                     }
-                    atkTimer = 0f;
+                    else
+                        tmp = lastCCPos;
                 }
-                else
-                    ani.Play(dPos.x < 0f ? "atk" : "atkFlipped");
-            }
+                break;
+            case 1:
+                if (timer <= 0)
+                {
+                    pysc.gravityScale = 0f;
+                    pysc.velocity = Vector2.zero;
+                    foreach (Collider2D c in GetComponents<Collider2D>())
+                        c.enabled = true;
+                    if (dPos.sqrMagnitude < atkDistance * atkDistance)
+                        CharCtrl.script.damage(atkDamage);
+                    curState = SEEKING;
+                }
+                break;
+            case 0:
+                transform.position += (Vector3)tmp * dashLerp;
+                tmp *= 1 - dashLerp;
+                if (tmp.x * tmp.x + tmp.y * tmp.y < 0.2f)
+                {
+                    tmp = Vector2.zero;
+                    if (dPos.sqrMagnitude < atkDistance * atkDistance)
+                        CharCtrl.script.damage(atkDamage);
+                    curState = SEEKING;
+                }
+                break;
         }
-        else
-        {
-            pysc.AddForce(Vector2.ClampMagnitude(-pysc.velocity, maxImpulse) * pysc.mass, ForceMode2D.Impulse);
-            if (stunTimer < 0f && deathTimer > deathTime)
-                ani.Play(dPos.x < 0f ? "idle" : "idleFlipped");
-        }
-        airTimer += Time.deltaTime;
-        if (atking == CRUSH && airTimer > crushAirTime)
-        {
-            pysc.gravityScale = 0f;
-            airTimer = 0;
-            atking = -1;
-            foreach (Collider2D c in GetComponents<Collider2D>())
-                c.enabled = true;
-            if (dPos.sqrMagnitude < atkDistance * atkDistance)
-                CharCtrl.script.damage(atkDamage);
-            atkTimer = 0f;
-        }
-        if (atking != -1 && atking != WINDUP && atking != CRUSH)
-        {
-            transform.position += (Vector3)dashPos * dashLerp;
-            dashPos *= 1 - dashLerp;
-            if (dashPos.x * dashPos.x + dashPos.y * dashPos.y < 1.0f)
-            {
-                atking = -1;
-                dashPos = Vector2.zero;
-                if (dPos.sqrMagnitude < atkDistance * atkDistance)
-                    CharCtrl.script.damage(atkDamage);
-            }
-            pysc.gravityScale = 0f;
-            atkTimer = 0f;
-        }*/
     }
     public override void kill(int damageType = 0)
     {
-        ani.Play(dPos.x <= 0 ? "death" : "deathFlipped");
-        deathTimer = deathTime;
+        ani.Play(lastCCPos.x <= 0 ? "death" : "deathFlipped");
+        timer = deathTime;
+        curState = DYING;
         foreach (Collider2D c in GetComponents<Collider2D>())
             c.enabled = false;
     }
     public override void damage(int amount, int damageType = 0)
     {
-        atkTimer = 0f;
-        agro = true;
-        if (deathTimer > deathTime)
+        if (curState != CRUSHING)
         {
-            ani.Play(dPos.x <= 0 ? "stagger" : "staggerFlipped");
-            stunTimer = stunTime;
+            curState = STUN;
+            timer = stunTime;
         }
         base.damage(amount, damageType);
     }
---
+++ Assets/scripts/cinamatics/Flash.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Flash : MonoBehaviour
+{
+    public float fade = 0.9f;
+    Color c;
+    UnityEngine.UI.Image img;
+    void Start()
+    {
+        img = GetComponent<UnityEngine.UI.Image>();
+        c = img.color;
+    }
+    void Update()
+    {
+        c.a *= fade;
+        img.color = c;
+        if (c.a < 0.05f)
+            Destroy(gameObject);
+    }
+}
