e6c47c80e1ab047e35e110e9c4befba936fb8225
jerry060599 <jerry060599@gmail.com>
2016-08-02 10:24:17 -0400

    Added scripts for better barriers

---
+++ Assets/scripts/Activatables/BarrierDestroy.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class BarrierDestroy : Activatable
+{
+    public float destroyAfter = 2f;
+    float killTimer = float.PositiveInfinity;
+    public override void activate(CharCtrl player)
+    {
+        killTimer = destroyAfter;
+        var e = GetComponent<ParticleSystem>().emission;
+        e.enabled = false;
+    }
+    void Update()
+    {
+        killTimer -= Time.deltaTime;
+        if (killTimer <= 0f)
+            Destroy(gameObject);
+    }
+}
