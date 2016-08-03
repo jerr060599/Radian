6613de394a9f3728de947f972c94b4189b31b3a0
jerry060599 <jerry060599@gmail.com>
2016-08-03 14:02:20 -0400

    Readded camera pan to turtle

--- Assets/scripts/game/EditorTools.cs
+++ Assets/scripts/game/EditorTools.cs
@@ -4,13 +4,10 @@ using System.Collections;
 public class EditorTools : MonoBehaviour
 {
     public bool resetPlayerPref = false;
-    public bool clearPrefOnStart = false;
     void Update()
     {
         if (!Application.isEditor || Application.isPlaying)
         {
-            if (clearPrefOnStart)
-                PlayerPrefs.DeleteAll();
             Destroy(this);
         }
         transform.position = Vector3.zero;
