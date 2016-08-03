e8d8461a9d12f8958f6f6727ae5c42cb42fb577d
jerry060599 <jerry060599@gmail.com>
2016-08-02 20:41:49 -0400

    Allow camera override for cinamatics

---
+++ Assets/scripts/cinamatics/CameraOverride.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraOverride : MonoBehaviour
+{
+    public Vector2 pos;
+    public float size, duration = 1f;
+    void OnTriggerEnter2D(Collider2D c)
+    {
+        if (c.gameObject == CharCtrl.script.gameObject)
+            CameraMovement.script.camOverride = this;
+    }
+    void OnTriggerExit2D(Collider2D c)
+    {
+        if (c.gameObject == CharCtrl.script.gameObject && CameraMovement.script.camOverride == this)
+            CameraMovement.script.camOverride = null;
+    }
+}
--- Assets/scripts/game/CameraMovement.cs
+++ Assets/scripts/game/CameraMovement.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class CameraMovement : MonoBehaviour
 {
     public static CameraMovement script = null;
+    public CameraOverride camOverride = null;
     GameObject c0;
     public float sizeTar = 140f, smooth = 0.06f, maxMouseOffset = 3f, mouseSensitivity = 0.005f;
     Vector3 shakePos = Vector3.zero, mousePos = Vector3.zero, curPos;
@@ -18,8 +19,14 @@ public class CameraMovement : MonoBehaviour
 
     void Update()
     {
+        camOverride.duration -= Time.deltaTime;
+        if (camOverride.duration <= 0f)
+            camOverride = null;
         mousePos += (Vector3.ClampMagnitude(new Vector3(Input.mousePosition.x - Screen.width / 2, Input.mousePosition.y - Screen.height / 2, 0f) * mouseSensitivity, maxMouseOffset) - mousePos) * smooth;
-        cam.transform.position = (curPos += (CharCtrl.script.gameObject.transform.position - curPos) * smooth) + shakePos + mousePos;
+        if (camOverride)
+            cam.transform.position = (curPos += ((Vector3)(camOverride.pos) - curPos) * smooth) + shakePos + mousePos;
+        else
+            cam.transform.position = (curPos += (CharCtrl.script.gameObject.transform.position - curPos) * smooth) + shakePos + mousePos;
         cam.transform.position = new Vector3(cam.transform.position.x, cam.transform.position.y, 0f);
         shakePos *= 0.7f;
         cam.orthographicSize += (sizeTar - (float)cam.orthographicSize) * smooth;
