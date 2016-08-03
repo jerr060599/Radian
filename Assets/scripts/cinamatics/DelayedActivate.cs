b6043907882b1189b57967d651b7702f6399e2f1
jerry060599 <jerry060599@gmail.com>
2016-08-03 19:55:01 -0400

    The ending is done. Its done.... Its done...

---
+++ Assets/scripts/cinamatics/DelayedActivate.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DelayedActivate : Activatable
+{
+    public float delay = 10f;
+    public override void init()
+    { }
+    public override void activate(CharCtrl player)
+    {
+        activated = true;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (activated)
+        {
+            delay -= Time.deltaTime;
+            if (delay <= 0)
+            {
+                chainedActivatable.activate(CharCtrl.script);
+                Destroy(gameObject);
+            }
+        }
+    }
+}
---
+++ Assets/scripts/cinamatics/PreEnding.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PreEnding : Activatable
+{
+    public GameObject tablet, boss;
+    public float timeToIpad = 10f;
+    public override void init()
+    {
+    }
+    public override void activate(CharCtrl player)
+    {
+        if (chainedActivatable)
+            chainedActivatable.activate(player);
+        tablet.transform.position = boss.transform.position;
+        tablet.SetActive(true);
+    }
+}
