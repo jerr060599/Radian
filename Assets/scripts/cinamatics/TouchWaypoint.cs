a23e1078af3e88e1628fa7386ea62a541cc1dc46
jerry060599 <jerry060599@gmail.com>
2016-08-05 12:57:28 -0400

    Added waypoint sound

--- Assets/scripts/cinamatics/TouchWaypoint.cs
+++ Assets/scripts/cinamatics/TouchWaypoint.cs
@@ -6,6 +6,9 @@ public class TouchWaypoint : Waypoint
     void OnTriggerEnter2D(Collider2D c)
     {
         if (c.gameObject == CharCtrl.script.gameObject)
+        {
+            SoundManager.script.playOn(transform, SoundManager.script.check);
             use();
+        }
     }
 }
