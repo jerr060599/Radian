f6d88608490572837c92729b73726120505a59fc
jerry060599 <jerry060599@gmail.com>
2016-08-11 20:10:32 -0700

    Preemtive bug fixing + audio fade

--- Assets/scripts/Activatables/BarrierDestroy.cs
+++ Assets/scripts/Activatables/BarrierDestroy.cs
@@ -5,9 +5,11 @@ public class BarrierDestroy : Activatable
     public float destroyAfter = 2f, lightFade = 0.7f;
     float killTimer = float.PositiveInfinity;
     Light[] larr = null;
+    AudioSource sauce = null;
     public override void init()
     {
         larr = GetComponentsInChildren<Light>();
+        sauce = GetComponent<AudioSource>();
         Debug.Log(larr.Length);
     }
     public override void activate(CharCtrl player)
@@ -23,9 +25,13 @@ public class BarrierDestroy : Activatable
     void FixedUpdate()
     {
         killTimer -= Time.fixedDeltaTime;
-        foreach (Light l in larr)
-            l.intensity *= lightFade;
         if (killTimer <= 0f)
             Destroy(gameObject);
+        else if (killTimer <= destroyAfter)
+        {
+            sauce.volume *= lightFade;
+            foreach (Light l in larr)
+                l.intensity *= lightFade;
+        }
     }
 }
