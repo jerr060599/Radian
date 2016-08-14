783ea467a9859351da4cb54ee7db1406fb907a1b
jerry060599 <jerry060599@gmail.com>
2016-08-14 11:21:12 -0700

    Added video player

---
+++ Assets/scripts/cinamatics/VideoPlayer.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+[RequireComponent(typeof(RawImage))]
+public class VideoPlayer : MonoBehaviour
+{
+    public MovieTexture mov;
+    public string lvl;
+    void Start()
+    {
+        mov.filterMode = FilterMode.Point;
+        GetComponent<RawImage>().texture = mov as MovieTexture;
+        GetComponent<RawImage>().texture.filterMode = FilterMode.Point;
+        GetComponent<AudioSource>().clip = mov.audioClip;
+        mov.Play();
+        GetComponent<AudioSource>().Play();
+    }
+    void Update()
+    {
+        if (!mov.isPlaying)
+        {
+            SceneManager.LoadScene(lvl);
+            Destroy(this);
+        }
+    }
+}
