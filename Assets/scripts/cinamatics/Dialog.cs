e97d4594aea0c17d61b0ee5fd591d2c971966c0a
jerry060599 <jerry060599@gmail.com>
2016-08-14 21:02:35 -0700

    Added npc dialogs

---
+++ Assets/scripts/cinamatics/Dialog.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dialog : Activatable
+{
+    public Sprite[] diags;
+    SpriteRenderer sr;
+    int index = 0;
+    public override void init()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+    public override void activate(CharCtrl player)
+    {
+        sr.sprite = diags[index];
+        index = Mathf.Min(index + 1, diags.Length - 1);
+    }
+    void OnTriggerExit2D(Collider2D c)
+    {
+        if (c.gameObject == CharCtrl.script.gameObject)
+        {
+            sr.sprite = null;
+            index = 0;
+        }
+    }
+}
